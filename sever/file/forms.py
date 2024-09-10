103579570df3911115d7bec8e6139c2434931f70
KangWoomin <blacksu25@gmail.com>
2024-09-10 14:03:56 +0900

    create_user_view

---
+++ sever/file/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import User
+
+class UserModelForm(forms.ModelForm):
+    password1 = forms.CharField(label='비밀번호', max_length=80, widget=forms.PasswordInput)
+    password2 = forms.CharField(label='비밀번호확인', max_length=80, widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['userID','email','birth','team']
+
+    def cleaned(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 and password2 and password2 != password1:
+            raise forms.ValidationError('비밀번호가 일치 하지 않습니다.')
+        
+        return password2
+    
+    def user_save(self, commit=True):
+        user = super().save(commit=False)
+        user = self.cleaned_data.get('password1')
+        
+        if commit:
+            user.save()
+        
+        return user
--- sever/file/views.py
+++ sever/file/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, logout
 # Create your views here.
+
+def main(request):
+    return HttpResponse('안녕')
+
+from .forms import UserModelForm
+def create_user(request):
+    if request.method =='POST':
+        form = UserModelForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+        
+    else:
+        form = UserModelForm()
+
+    return render(request,'create_user.html',{'form':form})
